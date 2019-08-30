20448b98dc648f2a3db951dd5d829ac88614a661
정연수 <jys1015@hanyang.co.kr>
2019-08-30 15:02:09 +0900

    2.5 using Babel

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-const express = require('express');
+//const express = require('express');
+// using babel
+import express from "express";
+
+
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
+//arrow function
+const handleListening = () =>
     console.log(`Listening on: https://localhost:${PORT}`);
-}
-
-function handleHome(req, res){
-    console.log(req);
-    res.send('Hello form Home');
-}
-
-function handleProfile(req, res){
-    res.send("You are on my profile");
-}
+const handleHome = (req, res) => res.send('Hello form Home');
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
+
+//npm install @babel/node 
+//npm install @bable/preset-env
+//npm install을할떄 -D를 붙이면 dependency에 들어가지않고 설치가 됨(devDependencies)
+// npm install nodemon -D
+//"start": "nodemon --exec babel-node index.js" 를하면 저장할때마다 다시 시작이됨 
