17862b6fee34180219350930aa2bf9f1c5ce5322
정연수 <jys1015@hanyang.co.kr>
2019-08-30 17:41:39 +0900

    2.11 MVC part3 setting controllers

---
+++ Controllers/videoController.js
@@ -0,0 +1,8 @@
+export const home = (req, res) => res.send("HOME");
+export const search = (req, res) => res.send("SEARCH");
+
+export const videos = (req, res) => res.send("videos");
+export const videoDetail = (req, res) => res.send("videoDetail");
+export const editVideo = (req, res) => res.send("editVideo");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
+export const uploadVideo = (req, res) => res.send("upload Video");
--- Router/vidoeRouter.js
+++ Router/vidoeRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes"
+import { videos, videoDetail, editVideo, deleteVideo, uploadVideo } from "../Controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, (req, res) => res.send('videos'));
-videoRouter.get(routes.videoDetail, (req, res) => res.send('videosDetail'));
-videoRouter.get(routes.editVideo, (req, res) => res.send('Edit Videos'));
-videoRouter.get(routes.deleteVideo, (req, res) => res.send('Delete Videos'));
-videoRouter.get(routes.videoUpload, (req, res) => res.send('Upload Videos'));
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoUpload, uploadVideo);;
 // 파일전체 export 변수앞에 붙으면 해당 변수만 export
 export default videoRouter;
