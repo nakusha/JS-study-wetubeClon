847b2e135393a047a3dd6e67c4390b6f7eb3c912
정연수 <jys1015@hanyang.co.kr>
2019-09-03 16:58:17 +0900

    2.17 using Template Variables in Pug from Controllers

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render("home");       // render .pug file name default folder path /views
-export const search = (req, res) => res.render("search");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });       // render .pug file name default folder path /views
+export const search = (req, res) => res.render("search", { pageTitle : "Search" });
 
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
-export const uploadVideo = (req, res) => res.render("uploadVideo");
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "VideoDetail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "EditVideo" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle : "Delete Video" });
+export const uploadVideo = (req, res) => res.render("uploadVideo", { pageTitle : "Upload Video" });
