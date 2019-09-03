6cc4d3f56e50cddf51d38858f9dab71842def5ee
정연수 <jys1015@hanyang.co.kr>
2019-09-03 15:27:53 +0900

    2.14 pug file added

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,8 +1,8 @@
 export const home = (req, res) => res.render("home");       // render .pug file name default folder path /views
-export const search = (req, res) => res.send("SEARCH");
+export const search = (req, res) => res.render("search");
 
-export const videos = (req, res) => res.send("videos");
-export const videoDetail = (req, res) => res.send("videoDetail");
-export const editVideo = (req, res) => res.send("editVideo");
-export const deleteVideo = (req, res) => res.send("deleteVideo");
-export const uploadVideo = (req, res) => res.send("upload Video");
+export const videos = (req, res) => res.render("videos");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const uploadVideo = (req, res) => res.render("uploadVideo");
