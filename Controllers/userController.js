f87d0af4da13521440663f26d4c87da0f84fd84d
정연수 <jys1015@hanyang.co.kr>
2019-09-04 16:13:54 +0900

    2.23 Join Controller / post method added / status code added in join controller

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,31 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+    res.render("join");
+};
+
+export const postJoin = (req, res) => {
+    //body parser에 의하여 정상적으로 console log에 찍힘
+    console.log(req.body);
+    const {
+        body:{name, email, password, password2}
+    } = req;
+
+    if (password !== password2){
+        res.status(400);
+        res.render("join", {pageTitle: "Join"});
+    }else{
+        // To Do: Register User
+        // To Do: Log user In
+        res.redirect(routes.home);
+    }
+    
+};
+
+export const login = (req, res) => {
+    res.render("login");
+};
+
 export const logout = (req, res) => res.render("logout");
 
 export const users = (req, res) => res.render("users");
--- Router/globalRouter.js
+++ Router/globalRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
 import routes from "../routes"
 import { home, search } from "../Controllers/videoController";
-import { join, logout, login } from "../Controllers/userController";
+import { getJoin, postJoin, logout, login } from "../Controllers/userController";
 
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
