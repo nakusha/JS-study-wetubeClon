5f260a3cb4781d6bbbfed5ad5d9a6f9e23ddc1fd
정연수 <jys1015@hanyang.co.kr>
2019-09-04 11:30:04 +0900

    2.18 Searching Controller form using get method parsing query

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,5 +1,18 @@
+import { runInNewContext } from "vm";
+
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });       // render .pug file name default folder path /views
-export const search = (req, res) => res.render("search", { pageTitle : "Search" });
+export const search = (req, res) => {
+    //ES 6 이전방식
+    //const searchingBy = req.query.term;
+    //ES 6 방식
+    // method가 get일경우에만 이렇게 접근가능
+    const {
+        // term의 이름을 바꿔줌
+        query:{ term:searchingBy }
+    } = req;
+    
+    res.render("search", { pageTitle: "search", searchingBy });
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle : "VideoDetail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle : "EditVideo" });
