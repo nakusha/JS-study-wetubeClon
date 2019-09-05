92b99d2ac288fd2824f51d7f4995102b97a0450b
정연수 <jys1015@hanyang.co.kr>
2019-09-05 17:03:03 +0900

    3.7 Upload Video File Path Repaire using express Static(will be useless)

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import { localsMiddleware } from "./middlewares";
 const app = express();
 
 app.set('view engine', "pug");      //npm install pug (View Engine)
+app.use("/uploads", express.static("uploads"))   // file을 전달하는 middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,7 @@ import routes from "./routes";
 import multer from "multer";
 
 //multer file을 올리면 path가 반환됨
-const multerVideo = multer({dest : "videos/"})
+const multerVideo = multer({dest : "uploads/videos/"})
 // pug에서 router에 접그한게 하기위한 middle ware
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = 'WeTube';
