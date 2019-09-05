ba1f985676ba333e8f4b7baefe6f668f59ddbb48
정연수 <jys1015@hanyang.co.kr>
2019-09-05 15:22:38 +0900

    3.4 Create Comment Model and link(join) to Video

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: "Text is required"
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }/*,
+    Video와 연결시키려면 comment에 video ID를 가지고있거나 Video에 Comment[]를 가지고 있어야함
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    }
+    */
+});
+
+const model = mongoose.model("Comment", CommentSchema);
+
+export defaultStatus model;
