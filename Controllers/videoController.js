d94b587cbd12186dfed1c617895555e38c7c86ce
정연수 <jys1015@hanyang.co.kr>
2019-09-06 15:23:56 +0900

    3.11 install eslint not work need more practice

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 //async
 export const home = async(req, res) => {
     try{
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({'_id': -1});
         res.render("home", { pageTitle: "Home", videos });       // render .pug file name default folder path /views
     }catch(error){
         console.log(error);
