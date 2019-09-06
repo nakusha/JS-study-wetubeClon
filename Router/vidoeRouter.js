bdef19eb0458342cd07d055fe2228e3dd9cd16d1
정연수 <jys1015@hanyang.co.kr>
2019-09-06 11:34:26 +0900

    3.9 Edit Video but not updated

--- Router/vidoeRouter.js
+++ Router/vidoeRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes"
-import { videoDetail, editVideo, deleteVideo, getUploadVideo, postUploadVideo } from "../Controllers/videoController";
+import { videoDetail, getEditVideo, postEditVideo, deleteVideo, getUploadVideo, postUploadVideo } from "../Controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -8,7 +8,9 @@ const videoRouter = express.Router();
 videoRouter.get(routes.videoUpload, getUploadVideo);;
 videoRouter.post(routes.videoUpload, uploadVideo, postUploadVideo);;
 
-videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
 videoRouter.get(routes.deleteVideo(), deleteVideo);
 videoRouter.get(routes.videoDetail(), videoDetail);
 // 파일전체 export 변수앞에 붙으면 해당 변수만 export
