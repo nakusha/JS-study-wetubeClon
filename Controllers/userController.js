ceb7740708718e7f89b4d529396e709067c327e4
정연수 <jys1015@hanyang.co.kr>
2019-09-10 16:21:37 +0900

    6.3 prepare login

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
@@ -5,7 +6,7 @@ export const getJoin = (req, res) => {
     res.render("join");
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
     //body parser에 의하여 정상적으로 console log에 찍힘
     console.log(req.body);
     const {
@@ -23,11 +24,11 @@ export const postJoin = async (req, res) => {
                 email
             });
             await User.register(user, password);
+            next();
         }catch(error){
             console.log(error);
+            res.redirect(routes.home);
         }
-        // To Do: Log user In
-        res.redirect(routes.home);
     }
     
 };
@@ -36,9 +37,10 @@ export const getLogin = (req, res) => {
     res.render("login");
 };
 
-export const postLogin = (req, res) => {
-    res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate('local', {
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
     // To Do: Process Logout
--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
 import userRouter from "./Router/userRouter";
 import videoRouter from "./Router/vidoeRouter";
 import globalRouter from "./Router/globalRouter";
 import routes from "./routes"
 import { localsMiddleware } from "./middlewares";
+import "./passport";
 
 const app = express();
 
@@ -21,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(helmet());              // 보안
 app.use(morgan("dev"));         // 로그
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 app.use(routes.home, globalRouter);
